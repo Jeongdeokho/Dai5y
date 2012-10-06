8c7fa27ed389e4b38c319d7e2e56994c9227a863
Dorimanx <yuri@bynet.co.il>
2012-10-06 04:24:04 +0200

    mxt224_u1: Introduce slide2wake by MyFlyxi! your legend will live with me :) Thanks allot :)
    Conflicts:
    	arch/arm/configs/dorimanx_defconfig
    	drivers/input/keyboard/gpio_keys.c
    	drivers/input/touchscreen/mxt224_u1.c

--- drivers/input/keyboard/gpio_keys.c
+++ drivers/input/keyboard/gpio_keys.c
@@ -29,6 +29,7 @@
 #ifdef CONFIG_FAST_BOOT
 #include <linux/wakelock.h>
 #endif
+#include <linux/i2c/mxt224_u1.h>
 
 extern struct class *sec_class;
 
@@ -899,6 +900,8 @@ static int __devinit gpio_keys_probe(struct platform_device *pdev)
 			wakeup = 1;
 
 		input_set_capability(input, type, button->code);
+
+		slide2wake_setdev(input);
 	}
 
 	error = sysfs_create_group(&pdev->dev.kobj, &gpio_keys_attr_group);
